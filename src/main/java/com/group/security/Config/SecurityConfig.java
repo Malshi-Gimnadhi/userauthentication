c351a243aa955ad5ecab4ad175ee1e871d028251
Malshi-Gimnadhi <malshigimnadhi@gmail.com>
2024-05-28 23:11:36 +0530

    5th commit

--- src/main/java/com/group/security/Config/SecurityConfig.java
+++ src/main/java/com/group/security/Config/SecurityConfig.java
@@ -60,5 +60,5 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
+//
 }
